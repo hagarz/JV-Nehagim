8f1f2f34a65fb39be33ce12d713c58b715f311c1
Hagar <hagarzem@gmail.com>
2020-08-03 19:06:17 +0300

    Fixed search field in admin

--- ey_drivers/drivers_app/admin.py
+++ ey_drivers/drivers_app/admin.py
@@ -6,12 +6,12 @@ from .models import Schedule
 class DriverAdmin(admin.ModelAdmin):
     fields = ['driver_id', 'driver_name']
     list_display = ('driver_name', 'driver_id')
-
+    search_fields = ['driver_id', 'driver_name']
 
 class ScheduleAdmin(admin.ModelAdmin):
     fields = ['driver_id', 'start_time', 'end_time']
     list_display = ('driver_id', 'date', "hours_worked_")
-    search_fields = ['start_time']
+    search_fields = ['driver_id__driver_name']
     #list_filter = ['date']
 
 
